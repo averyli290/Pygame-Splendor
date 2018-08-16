5101a237d923d39b18336f8085b1b5d522170516
Isaac Li <ili@e1z2r2p1.42.us.org>
2018-08-16 14:54:26 -0700

    Move to general_functions

--- general_functions.py
+++ general_functions.py
@@ -1,3 +1,7 @@
+import pygame
+
+pygame.font.init()
+
 def get_colors():
     colors_dict = {}
     
@@ -11,3 +15,15 @@ def get_colors():
         colors_dict[color] = rgb_val
 
     return colors_dict
+
+def text_objects(text, font, color, rotation=0):
+    textSurface = font.render(text, True, color)
+    textSurface = pygame.transform.rotate(textSurface, rotation)
+    return textSurface, textSurface.get_rect()
+
+def display_text(text, size, font, color, italic=False, rotation=0):
+    torender = pygame.font.Font(font, size)
+    torender.set_italic(italic)
+    TextSurf, TextRect = text_objects(text, torender, color, rotation)
+    TextRect.center = (0, 0) 
+    return TextSurf, TextRect
